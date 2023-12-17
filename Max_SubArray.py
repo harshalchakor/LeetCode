75c93d6aee01b13093c8fcd75f319d5bfe0b9d7e
harshalchakor <harshalchakor1@gmail.com>
2023-12-17 15:03:17 -0800

    Create Max_SubArray.py

---
+++ Max_SubArray.py
@@ -0,0 +1,20 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+class Solution:
+    def MaxSubArray(self, nums) -> int:
+        print(nums)
+        left = 0
+        maxSub = 0
+        currSum = 0
+        for i, n in enumerate(nums):
+            if currSum < 0:
+                currSum = 0
+                left = i
+            currSum += n
+            right = i
+            maxSub = max(maxSub, currSum)
+        return maxSub, nums[left:right]
+
+sol = Solution()
+print(sol.MaxSubArray([-1, -3, 5, 8, 8, -20]))
