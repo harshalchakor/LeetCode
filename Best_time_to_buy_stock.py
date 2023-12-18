a6ecc39c20f6f681f03df60c07d91f7009cb525e
harshalchakor <harshalchakor1@gmail.com>
2023-12-18 12:23:15 -0800

    Create Best_time_to_buy_stock.py

---
+++ Best_time_to_buy_stock.py
@@ -0,0 +1,18 @@
+class Answer:
+    def maxProfit(self, prices) -> int:
+        
+        l, r = 0, 1
+        maxProfit = 0
+        
+        while r < len(prices):
+            if prices[r] > prices[l]:
+                currProfit = prices[r] - prices[l]
+                maxProfit = max(maxProfit, currProfit)
+            else:
+                l = r
+            r += 1
+            
+        return maxProfit
+
+ans = Answer()
+print(ans.maxProfit([7, 1, 5, 3, 6, 4]))
