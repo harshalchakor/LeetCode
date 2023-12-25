f48421a0eb1d6a96e3e39b1180a262ec7a0bc415
harshalchakor <harshalchakor1@gmail.com>
2023-12-25 14:36:39 -0800

    Update MejorityElement.py

--- MejorityElement.py
+++ MejorityElement.py
@@ -10,6 +10,16 @@ class Answer:
             maxCount = max(maxCount, count[n])
             res = n if  maxCount > count[res] else res
         return res
+        
+        # Solution 2. Only will work when element is occured more than half times the length of array. 
+        
+        count, res = 1, nums[0]
+        
+        for n in nums:
+            if count == 0:
+                res = n
+            count = (count + 1) if n == res else (count - 1)
+        return res
     
 ans = Answer()
-print(ans.MejorityElement([1, 6, 6, 4, 5, 5, 5]))
+print(ans.MejorityElement([1, 6, 6, 4, 5, 5, 6, 6]))
