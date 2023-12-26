016cd98dac367d2325dc192feef5658dc34efc9d
harshalchakor <harshalchakor1@gmail.com>
2023-12-25 16:48:42 -0800

    Create squareOfSortedArray.py

---
+++ squareOfSortedArray.py
@@ -0,0 +1,22 @@
+# Print the squares of the sorted list
+
+class Answer():
+    
+    def squareOfSortedArray(self, nums):
+        res = []
+        l, r = 0, len(nums) -1
+        
+        while l <= r:
+            if nums[l] * nums[l] > nums[r] * nums[r]:
+                res.append(nums[l] * nums[l])
+                l += 1
+                
+            else:
+                res.append(nums[r] * nums[r])
+                r -= 1
+        
+        return res[::-1]
+    
+    
+ans = Answer()
+print(ans.squareOfSortedArray([-8, -5, -3, 0, 1, 4, 6]))
