fdbd2e643b70b6b072271d04d644c2f723ece798
harshalchakor <harshalchakor1@gmail.com>
2024-01-08 23:45:04 -0800

    Update maxProductSubArray.py

--- maxProductSubArray.py
+++ maxProductSubArray.py
@@ -5,8 +5,9 @@ class Answer():
         curMax, curMin = 1, 1
         
         for n in nums:
+            tmp = n * curMax
             curMax = max(n * curMax, n * curMin, n)
-            curMin = min(n * curMax, n * curMin, n)
+            curMin = min(tmp, n * curMin, n)
             
             res = max(res, curMax)
         
