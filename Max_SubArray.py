6ecdbf9bb413b3006b666806a2948425e6fdd9ad
harshalchakor <harshalchakor1@gmail.com>
2024-01-08 17:22:52 -0800

    Update Max_SubArray.py

--- Max_SubArray.py
+++ Max_SubArray.py
@@ -5,7 +5,7 @@ class Solution:
     def MaxSubArray(self, nums) -> int:
         print(nums)
         left = 0
-        maxSub = 0
+        maxSub = nums[0]
         currSum = 0
         for i, n in enumerate(nums):
             if currSum < 0:
