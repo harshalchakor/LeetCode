579fa4f80bff3a7be5ff4d865add63bcf706813b
harshalchakor <harshalchakor1@gmail.com>
2024-01-08 17:35:27 -0800

    Create maxProductSubArray.py

---
+++ maxProductSubArray.py
@@ -0,0 +1,18 @@
+class Answer():
+    def maxProductSubArray(self, nums):
+        
+        res = max(nums)
+        maxSub, minSub = 1, 1
+        
+        for n in nums:
+            maxSub = max(n * maxSub, n * minSub, n)
+            minSub = min(n * maxSub, n * minSub, n)
+            res = max(res, maxSub, minSub)
+        
+        return res
+    
+ans = Answer()
+print(ans.maxProductSubArray([2, 3, 4, 1]))
+print(ans.maxProductSubArray([4, -3, 10]))
+print(ans.maxProductSubArray([0, 4, -100, -5]))
+print(ans.maxProductSubArray([-2, -3, 4, 1]))
