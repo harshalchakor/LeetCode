d9d67e192f1ae3706d2f641434fe21975026df1c
harshalchakor <harshalchakor1@gmail.com>
2024-01-10 21:24:37 -0800

    Create countingBits.py

---
+++ countingBits.py
@@ -0,0 +1,21 @@
+# Number of 1 bits in integer number
+class Answer():
+    def countingBits(self, n):
+        
+        dp = [0] * (n + 1)
+        offset = 1
+        
+        for i in range(1, n + 1):
+            if i == offset * 2:
+                offset = i
+                
+            dp[i] = 1 + dp[i - offset]
+            
+        return dp
+                
+ans = Answer()
+print(ans.countingBits(3))
+print(ans.countingBits(7))
+print(ans.countingBits(8))
+print(ans.countingBits(16))
+print(ans.countingBits(31))
